aa56e29ecca4a6535b13acb37c49e9775fbcd907
rissuuuu <rissuuuu@gmail.com>
2020-06-17 23:07:25 +0530

    News APP

--- app/src/main/java/com/appyhigh/newsfeed/WebV.java
+++ app/src/main/java/com/appyhigh/newsfeed/WebV.java
@@ -3,12 +3,16 @@ package com.appyhigh.newsfeed;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
-public class WebView extends AppCompatActivity {
-
+public class WebV extends AppCompatActivity {
+WebView wv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
+        wv=findViewById(R.id.web_v);
+
+
     }
 }
