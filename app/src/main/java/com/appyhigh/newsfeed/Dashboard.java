9ecf7ca33453035d7f8ef5800f3b9366e9c1e5d6
rissuuuu <rissuuuu@gmail.com>
2020-06-18 21:54:04 +0530

    Newsfeed

--- app/src/main/java/com/appyhigh/newsfeed/Dashboard.java
+++ app/src/main/java/com/appyhigh/newsfeed/Dashboard.java
@@ -31,10 +31,9 @@ import com.onesignal.OneSignal;
 import java.util.List;
 import java.util.Locale;
 
-public class Dashboard extends AppCompatActivity implements View.OnClickListener {
+public class Dashboard extends AppCompatActivity implements View.OnClickListener,LocationListener {
     CardView global, countries;
-    Double latitude = 0.0;
-    Double longitude = 0.0;
+
     Location gps_loc = null, network_loc = null, final_loc = null;
     LocationManager locationManager;
 
@@ -59,14 +58,84 @@ public class Dashboard extends AppCompatActivity implements View.OnClickListener
                     android.Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
         }
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
+        locationEnabled();
+        getLocation();
         global = findViewById(R.id.id_global);
         countries = findViewById(R.id.id_countries);
         global.setOnClickListener(this);
         countries.setOnClickListener(this);
 
     }
+    private void locationEnabled() {
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        boolean gps_enabled = false;
+        boolean network_enabled = false;
+        try {
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (!gps_enabled && !network_enabled) {
+            new AlertDialog.Builder(Dashboard.this)
+                    .setTitle("Enable GPS Service")
+                    .setMessage("We need your GPS location to show Near Places around you.")
+                    .setCancelable(false)
+                    .setPositiveButton("Enable", new
+                            DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                                    startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                                }
+                            })
+                    .setNegativeButton("Cancel", null)
+                    .show();
+        }
+    }
 
+    void getLocation() {
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 500, 5, (LocationListener) this);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        try {
+            Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+            String locality=addresses.get(0).getLocality();
+            String Area=addresses.get(0).getAdminArea();
+            String country=addresses.get(0).getCountryName();
+            String postalcode=addresses.get(0).getPostalCode();
+            String addressline=addresses.get(0).getAddressLine(0);
+            Toast.makeText(getApplicationContext(),country,Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 
 
     @Override
@@ -77,8 +146,6 @@ public class Dashboard extends AppCompatActivity implements View.OnClickListener
             switch (v.getId()) {
 
                 case R.id.id_global:
-                    Toast.makeText(getApplicationContext(), longitude.toString() + " " + latitude.toString(), Toast.LENGTH_SHORT).show();
-
                     startActivity(new Intent(Dashboard.this, Global.class));
                     break;
                 case R.id.id_tech:
